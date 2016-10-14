65cbbc48d85c0126c2d5f57f988dae446b166442
Robert Brockie <robertbrockie@gmail.com>
2016-10-13 23:11:01 -0400

    Simple Ubuntu/MySQL VM.

---
+++ README.md
@@ -0,0 +1,4 @@
+vagrant-mysql
+============
+
+A simple Ubuntu box with MySQL setup.
---
+++ provision.sh
@@ -0,0 +1,25 @@
+#! /usr/bin/env bash
+
+# DB Variables
+DBUSER=root
+DBPASSWD=foobar
+
+echo -e "\n--- Create a simple Ubuntu/MySQL VM... ---\n"
+
+echo -e "\n--- Updating packages list ---\n"
+apt-get -qq update
+
+echo -e "\n--- Install base packages ---\n"
+apt-get -y install vim curl build-essential python-software-properties git > /dev/null 2>&1
+
+echo -e "\n--- Install MySQL specific packages and settings ---\n"
+echo "mysql-server mysql-server/root_password password $DBPASSWD" | debconf-set-selections
+echo "mysql-server mysql-server/root_password_again password $DBPASSWD" | debconf-set-selections
+echo "phpmyadmin phpmyadmin/dbconfig-install boolean true" | debconf-set-selections
+echo "phpmyadmin phpmyadmin/app-password-confirm password $DBPASSWD" | debconf-set-selections
+echo "phpmyadmin phpmyadmin/mysql/admin-pass password $DBPASSWD" | debconf-set-selections
+echo "phpmyadmin phpmyadmin/mysql/app-pass password $DBPASSWD" | debconf-set-selections
+echo "phpmyadmin phpmyadmin/reconfigure-webserver multiselect none" | debconf-set-selections
+apt-get -y install mysql-server-5.5 mysql-client-core-5.5 > /dev/null 2>&1
+
+echo -e "\n--- Donzo! ---\n"
